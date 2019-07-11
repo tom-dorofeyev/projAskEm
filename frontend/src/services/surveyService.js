207a9b973271582b55a9975e244652f0e3b3b1cc
tom dorofeyev <[tomdorofey@gmail.com]>
2019-07-11 17:20:04 +0300

    added DB JS

--- frontend/src/services/surveyService.js
+++ frontend/src/services/surveyService.js
@@ -1,3 +1,8 @@
 export default {
 
 }
+
+import DB from '../data/surveyDB'
+
+
+console.log(DB.surveys)
--- frontend/src/views/homePage.vue
+++ frontend/src/views/homePage.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="home-page">
     <router-link to="/survey/create">MAKE SURVEY</router-link>
+    
   </div>
 </template>
 
 <script>
-
+import surveyService from '@/services/surveyService'
 
 export default {
   components: {
