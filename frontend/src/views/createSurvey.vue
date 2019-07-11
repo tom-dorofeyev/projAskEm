8c282cfee14c837ffddc9154ba1c9a3bbfd9629c
tom dorofeyev <[tomdorofey@gmail.com]>
2019-07-11 17:51:34 +0300

    adding inputs

--- frontend/src/views/createSurvey.vue
+++ frontend/src/views/createSurvey.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="create-survey-page">
-    MAKING A SURVEY
+    <h1>create Survey</h1>
+    <section class="quest-editot-list" v-if="quests.length > 0">
+      <div v-for="currQuest in quests"
+            :key="currQuest.title"
+            class="quest-editor">
+        choose question type:
+        <select v-model="quests.type">
+          <option value="textBox">Text Box</option>
+          <option value="singleAnswer">Single Answer</option>
+          <option value="multAnswer">Multiple Answers</option>
+        </select>
+      </div>
+    </section>
+
+    {{quests}}
+
+    <button @click="addQuest">Add Question</button>
   </div>
 </template>
 
@@ -8,6 +24,20 @@
 
 
 export default {
+  data(){
+    return{
+      quests:[]
+    }
+  },
+  methods:{
+    addQuest(){
+      this.quests.push({
+        title: '',
+        type:'',
+        opts:[]
+      })
+    }
+  },
   components: {
 
   }
