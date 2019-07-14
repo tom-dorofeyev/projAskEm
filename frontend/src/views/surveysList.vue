29f84f21d98fa13110d36c5c29390e1f7f684893
Nadav Abuhatsira <nudu22@gmail.com>
2019-07-14 14:55:47 +0300

    New Survey List Setup

--- frontend/src/views/surveysList.vue
+++ frontend/src/views/surveysList.vue
@@ -1,8 +1,23 @@
 
 <template>
   <div class="surveys-list-page" v-if="surveys.length > 0">
-    <div class="survey-results-container" v-for="currSurvey in surveys" :key="currSurvey._id">
-      <router-link :to="getSurveyUrl(currSurvey._id)">{{currSurvey.name}}</router-link>
+    <div>
+      <div class="survey-preview-container" v-for="currSurvey in surveys" :key="currSurvey._id">
+        <h3 class="survey-preview-title">{{currSurvey.name}}</h3>
+        <p class="survey-preview-timestamp">{{currSurvey.createdAt}}</p>
+        <section v-if="currSurvey.tags.length">
+          <div
+            class="survey-preview-tags"
+            v-for="(tag, tagIdx) in currSurvey.tags"
+            :key="tagIdx"
+          >#{{tag}}</div>
+        </section>
+        <h5 class="survey-preview-quest-counter">{{currSurvey.quests.length}} Questions</h5>
+        <br />
+        <router-link class="survey-preview-btn" :to="getSurveyUrl(currSurvey._id)">Enter Survey</router-link>
+        <br />
+        <br />
+      </div>
     </div>
   </div>
 </template>
@@ -10,18 +25,18 @@
 <script>
 export default {
   data: () => ({
-    surveys: [],
+    surveys: []
   }),
   created() {
     (async () => {
-      await this.$store.dispatch({type:'loadSurveys'})
+      await this.$store.dispatch({ type: "loadSurveys" });
       this.surveys = this.$store.getters.getSurveyList;
     })();
   },
   methods: {
-    getSurveyUrl(id){
-      return `/survey/${id}`
+    getSurveyUrl(id) {
+      return `/survey/${id}`;
     }
-  },
+  }
 };
 </script>
