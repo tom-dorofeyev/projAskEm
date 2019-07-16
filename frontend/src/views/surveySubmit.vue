020f07e450e8229339d55301c6b713f0c5485c4b
tom dorofeyev <[tomdorofey@gmail.com]>
2019-07-16 22:09:57 +0300

    minor bug fix

--- frontend/src/views/surveySubmit.vue
+++ frontend/src/views/surveySubmit.vue
@@ -27,11 +27,6 @@
 import questSubmit from '@/components/questSubmit'
 import io from 'socket.io-client';
 
-var socket = io.connect('http://localhost');
-  socket.on('news', function (data) {
-    console.log(data);
-    socket.emit('my other event', { my: 'data' });
-});
 
 export default {
   data: () => ({
