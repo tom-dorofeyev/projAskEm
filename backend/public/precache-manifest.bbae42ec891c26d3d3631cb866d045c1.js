dc483c8526f57ebc27b2fcb601a5b12367da1a6e
tom dorofeyev <[tomdorofey@gmail.com]>
2019-07-20 17:54:14 +0300

    ran build into backend public dir

---
+++ backend/public/precache-manifest.bbae42ec891c26d3d3631cb866d045c1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb74b49ae5a3263e45e83c72d837b58e",
+    "url": "/img/nofar.eb74b49a.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "7b85de475485547ab94bea0dcf6a8f4f",
+    "url": "/img/aboutus.7b85de47.jpg"
+  },
+  {
+    "revision": "4c69f71137795dbb2077",
+    "url": "/js/chunk-2d0dd11b.7407df9a.js"
+  },
+  {
+    "revision": "ae472f8952205507e3f3",
+    "url": "/js/chunk-7a5714b4.cda880c4.js"
+  },
+  {
+    "revision": "29abb01c8d95a0935cb7",
+    "url": "/js/chunk-vendors.fadc0f72.js"
+  },
+  {
+    "revision": "9f3702b544b4646b9a2f",
+    "url": "/js/chunk-2d2308d0.34a83bb3.js"
+  },
+  {
+    "revision": "c59dc89f09bc93c715a736a9132f27f7",
+    "url": "/fonts/Yellowtail-Regular.c59dc89f.ttf"
+  },
+  {
+    "revision": "31753003b2b1e9e413ce983625af9547",
+    "url": "/img/nadav.31753003.jpg"
+  },
+  {
+    "revision": "8df328359625fb9b95b720c8dd34df22",
+    "url": "/img/tom.8df32835.jpg"
+  },
+  {
+    "revision": "691d136a816940a6065f",
+    "url": "/js/app.845fefc5.js"
+  },
+  {
+    "revision": "c79710d5dc1360490b88d9f1b2cae8ac",
+    "url": "/img/twitter.c79710d5.svg"
+  },
+  {
+    "revision": "c75b36857202658a348397c389e54807",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f52fa1da4e782721e88f4d3aa6371ce8",
+    "url": "/img/linkedin.f52fa1da.svg"
+  },
+  {
+    "revision": "92d130c4d3bf80989bf44b0ece074223",
+    "url": "/img/homepage-background.92d130c4.jpg"
+  },
+  {
+    "revision": "b53ff55e09a092f9f0cbbf74af01b715",
+    "url": "/img/github.b53ff55e.svg"
+  },
+  {
+    "revision": "d6408c93ff19cf5ee2c4d49dd3298c82",
+    "url": "/img/facebook.d6408c93.svg"
+  },
+  {
+    "revision": "e6cb38c7ef38c6b7ada976f334d69843",
+    "url": "/img/askem.e6cb38c7.png"
+  },
+  {
+    "revision": "84abe14c9756256a4b91300ba3e4ec62",
+    "url": "/fonts/Raleway-Regular.84abe14c.ttf"
+  },
+  {
+    "revision": "691d136a816940a6065f",
+    "url": "/css/app.35a42bd9.css"
+  }
+];
