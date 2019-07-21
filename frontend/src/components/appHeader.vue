a45bee424b10377605e873669c2bd74a99d8f9ad
tom dorofeyev <[tomdorofey@gmail.com]>
2019-07-21 22:01:17 +0300

    header done

--- frontend/src/components/appHeader.vue
+++ frontend/src/components/appHeader.vue
@@ -1,12 +1,16 @@
 <template>
     <div class="app-header-div flex">
-    <nav class="main-nav flex">
-      <router-link to="/"><img src="@/assets/images/askem.png" alt="Link to home page"></router-link>
-      <router-link to="/about">About</router-link> 
-      <router-link to="/survey/list">Survey List</router-link> 
-      <router-link to="/signup">Sign up</router-link>
-      <router-link v-if="!user.userName" to="/login" id="sign-in">Sign In</router-link>
-    </nav>
+      <nav class="main-nav flex">
+        <div class="logo">
+          <router-link to="/"><img src="@/assets/images/askem.png" alt="Link to home page"></router-link>
+        </div>
+        <div class="router-link-list flex">
+          <router-link to="/about">About</router-link> 
+          <router-link to="/survey/list">Survey List</router-link> 
+          <router-link to="/signup">Sign up</router-link>
+          <router-link v-if="!user.userName" to="/login" id="sign-in">Sign In</router-link>
+        </div>
+      </nav>
       <nav class="user-main-nav flex" v-if="user.userName">
         <router-link to="/profile">User Details</router-link>
         <button @click="doLogout">Logout</button>
