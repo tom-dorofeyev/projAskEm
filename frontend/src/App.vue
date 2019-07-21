a769fc42f6c5babd45b878ef67c2d1ece0b63987
tom dorofeyev <[tomdorofey@gmail.com]>
2019-07-21 19:55:43 +0300

    fixed scss mistake

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -18,6 +18,5 @@ export default {
 }
 </script>
 
-<style lang="scss">
-@import "../src/styles/global.scss";
+<style lang="scss" src="@/styles/global.scss">
 </style>
--- frontend/src/components/appHeader.vue
+++ frontend/src/components/appHeader.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="app-header-div flex">
-    <router-link to="/"><img src="@/assets/images/askem.png" alt="Link to home page"></router-link>
     <nav class="main-nav flex">
+      <router-link to="/"><img src="@/assets/images/askem.png" alt="Link to home page"></router-link>
       <router-link to="/about">About</router-link> 
       <router-link to="/survey/list">Survey List</router-link> 
       <router-link to="/signup">Sign up</router-link>
-      <router-link v-if="!user.userName" to="/login">Sign In</router-link>
+      <router-link v-if="!user.userName" to="/login" id="sign-in">Sign In</router-link>
     </nav>
       <nav class="user-main-nav flex" v-if="user.userName">
         <router-link to="/profile">User Details</router-link>
@@ -37,3 +37,9 @@ export default {
 
 }
 </script>
+
+
+
+<style lang="scss" scoped src="@/styles/components/_appHeader.scss">
+
+</style>
