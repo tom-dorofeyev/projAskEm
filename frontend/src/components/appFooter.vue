44f6e1efd206900917e076e9f6b248eb18bc6d5c
Nadav Abuhatsira <nudu22@gmail.com>
2019-07-26 15:23:12 +0300

    Added Favicon + Created Basic Social Sites and linked to buttons

--- frontend/src/components/appFooter.vue
+++ frontend/src/components/appFooter.vue
@@ -2,9 +2,9 @@
   <section class="footer-section">
       <div class="follow-us flex">
         <h5>Follow Us</h5>
-          <a href="https://twitter.com/"> <img src="@/assets/images/socialNetworkIcons/twitter.png" alt="Twitter Link" /></a>
-          <a href="https://www.facebook.com/"><img src="@/assets/images/socialNetworkIcons/facebook.png" alt="Facebook Link" /></a> 
-          <a href="https://www.linkedin.com/"><img src="@/assets/images/socialNetworkIcons/linkedin.png" alt="Linkedin Link" /></a>
+          <a href="https://twitter.com/AskEmSurveys"> <img src="@/assets/images/socialNetworkIcons/twitter.png" alt="Twitter Link" /></a>
+          <a href="https://fb.me/AskEmSurveys"><img src="@/assets/images/socialNetworkIcons/facebook.png" alt="Facebook Link" /></a> 
+          <a href="https://www.linkedin.com/company/askemsurveys"><img src="@/assets/images/socialNetworkIcons/linkedin.png" alt="Linkedin Link" /></a>
       </div>
     <h6>© Copy Rights. All Rights Reserved to NTN | Designed by NTN.</h6>
   </section>
