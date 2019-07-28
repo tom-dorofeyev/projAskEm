c292975d72038bf73ff8bcb06f9a959038afe8dd
Nadav Abuhatsira <nudu22@gmail.com>
2019-07-28 13:43:31 +0300

    fixed survey title on survey sumbit

---
+++ frontend/src/components/qrCode.vue
@@ -0,0 +1,31 @@
+<script>
+import { Bar } from "vue-chartjs";
+
+export default {
+  props: ["labels", "label", "data"],
+  extends: Bar,
+  mounted() {
+    // Overwriting base render method with actual data.
+    this.renderMyChart();
+  },
+  methods: {
+    renderMyChart() {
+      this.renderChart({
+        labels: this.labels,
+        datasets: [
+          {
+            label: "people answered",
+            backgroundColor: "#f87979",
+            data: this.data
+          }
+        ]
+      });
+    }
+  },
+  watch:{
+    data(){
+      this.renderMyChart();
+    }
+  }
+};
+</script>
