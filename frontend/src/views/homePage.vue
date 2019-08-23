19b8f9f3d406537e99f4de9f564e512e7c58169a
tom dorofeyev <[tomdorofey@gmail.com]>
2019-08-23 13:17:33 +0300

    removed "how it workd"

--- frontend/src/views/homePage.vue
+++ frontend/src/views/homePage.vue
@@ -13,20 +13,13 @@
         </router-link>
         </div>
       </div>
-        <section class="trending-surveys title">
+      <section class="trending-surveys title">
         <div class="under-title"></div>
         <h2 class="trending-surveys-title">Most Trending Surveys</h2>
-      <section class="survey-list top-answered">
-        <survey-preview v-for="currSurvey in popularSurveys" :key="currSurvey._id" :survey="currSurvey"/>
-      </section>
+        <section class="survey-list top-answered">
+          <survey-preview v-for="currSurvey in popularSurveys" :key="currSurvey._id" :survey="currSurvey"/>
         </section>
-      <div class="how-it-works">
-      <div class="under-title"></div>
-        <h2 class="how-it-works-title">How It Works</h2>
-        <div class="how-it-work-gif-div flex">
-          <h3>Make a surveys, Publish him, Track the results live and Watch the world answer! </h3>
-        </div>
-      </div>
+      </section>
       <div class="use-our-service">
       <div class="under-title"></div>
         <h2 class="companies-use-us">Who Works With Us?</h2>
